781d1bf52d26e542b6df2d0659fda508e13cc907
wylietroy <wyliesebtroy@gmail.com>
2023-10-10 15:46:48 -0400

    fixed three bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+import { act } from '@react-three/fiber';
 
 const Navbar = () => {
+  const [active, setActive] = useState('`')
+
   return (
-    <div>Navbar</div>
+    <nav 
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}>
+          <img src={logo} alt={logo} className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Wylie <span className="sm:block hidden"> | Front End Developer</span></p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((Link) => (
+            <li
+              key={Link.id}
+              className={`${
+                active === Link.title
+                  ? "text-white"
+                  : "text-secondary" 
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(Link.title)}
+            >
+              <a href={`#${Link.id}`}>{Link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </nav>
   )
 }
 
