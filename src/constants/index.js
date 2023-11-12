e8b4957854df5c785cf607f083e0f875dc1f2d4d
wylietroy <wyliesebtroy@gmail.com>
2023-11-12 18:38:08 -0500

    created code for feedback page, will add when gathered professional feedbacks

--- src/constants/index.js
+++ src/constants/index.js
@@ -172,27 +172,27 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
+        "Enter Testimonials Here.",
+      name: "Enter Name Here",
+      designation: "Enter Position Here",
+      company: "Enter Company Here",
+      image: "Enter image Here Optional",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
+        "Enter Testimonials Here.",
+      name: "Enter Name Here",
+      designation: "Enter Position Here",
+      company: "Enter Company Here",
+      image: "Enter image Here Optional",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
+        "Enter Testimonials Here.",
+      name: "Enter Name Here",
+      designation: "Enter Position Here",
+      company: "Enter Company Here",
+      image: "Enter image Here Optional",
     },
   ];
   
